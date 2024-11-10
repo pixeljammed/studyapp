1964b60a86cce453b9001821faa7a7a38a47f0a0
pixeljam <MT156314@hrsfc.ac.uk>
2024-11-10 00:16:40 +0000

    BROKEN - fonts added

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -17,5 +17,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    fontSize: 24,
+    fontFamily: 'Futura',
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
 import { Stack } from 'expo-router';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'Builder Sans': require('./assets/fonts/BuilderSans-Regular-400'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
