a75735b9a6ba6d3e646db8fe3363c65324eb1ef1
pixeljam <MT156314@hrsfc.ac.uk>
2024-11-10 23:00:07 +0000

    Custom fonts, push to github

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,8 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.title}>settings</Text>  
+      <Text style={styles.text}>coming soon!</Text>
     </View>
   );
 }
@@ -15,9 +16,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 72,
+    fontFamily: 'BEExtraBold',
+  },
   text: {
     color: '#fff',
     fontSize: 24,
-    fontFamily: 'BuilderSans-Regular',
+    fontFamily: 'BSLight',
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,34 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
  const [loaded, error] = useFonts({
-   'Regular': require('@/assets/fonts/Builder Sans Regular.otf'),
+    /// FONTS /// - https://imgur.com/a/8Gs6BSY
+    // Builder Sans
+    'BSThin': require('@/assets/fonts/BuilderSansThin.otf'),
+    'BSLight': require('@/assets/fonts/BuilderSansLight.otf'),
+    'BSRegular': require('@/assets/fonts/BuilderSansRegular.otf'),
+    'BSMedium': require('@/assets/fonts/BuilderSansMedium.otf'),
+    'BSSemibold': require('@/assets/fonts/BuilderSansSemiBold.otf'),
+    'BSBold': require('@/assets/fonts/BuilderSansBold.otf'),
+    'BSExtraBold': require('@/assets/fonts/BuilderSansExtraBold.otf'),
+
+    // Builder Extended
+    'BELight': require('@/assets/fonts/BuilderExtendedLight.otf'),
+    'BERegular': require('@/assets/fonts/BuilderExtendedRegular.otf'),
+    'BESemibold': require('@/assets/fonts/BuilderExtendedSemiBold.otf'),
+    'BEBold': require('@/assets/fonts/BuilderExtendedBold.otf'),
+    'BEExtraBold': require('@/assets/fonts/BuilderExtendedExtraBold.otf'),
+
+    // Builder Mono
+    'BMLight': require('@/assets/fonts/BuilderMonoLight.otf'),
+    'BMRegular': require('@/assets/fonts/BuilderMonoRegular.otf'),
+    'BMBold': require('@/assets/fonts/BuilderMonoBold.otf'),
+
+
+
+    // Fallbacks
+    'Sans': require('@/assets/fonts/SourceSans.ttf'),
+    'Extended': require('@/assets/fonts/Montserrat.ttf'),
+    'Mono': require('@/assets/fonts/RobotoMono.ttf'),
  });
 
  useEffect(() => {
